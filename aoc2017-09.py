c352f542fcc371cc4fc2f13e0e03bd34436858ff
annaoskarson <anna.oskarson@gmail.com>
2021-10-16 12:24:55 +0200

    Dag 9.

---
+++ aoc2017-09.py
@@ -0,0 +1,31 @@
+fil = open('aoc2017-09-input.txt', 'r')
+ls = fil.read()
+
+i, score, sum, garb = 0, 0, 0, 0
+while i < len(ls):
+    if ls[i] =='<': # Skräp börjar, ta bort allt skräp.
+        while ls[i] != '>':
+            if ls[i] == '!':
+                i += 1
+                garb -= 1 # Räkna inte nästa tecken
+            i += 1
+            garb += 1
+        garb -= 1 # Räkna inte avslutande >
+    if ls[i] == '{': # Ny grupp börjar, öka score
+        score += 1
+    if ls[i] == '}': # Grupp slutar, räkna poäng, hoppa ner en score
+        sum += score
+        score -= 1
+    i += 1
+
+
+def partone():
+    print("Advent of Code 2017, day 7, part 1.")
+    print("The answer is:", sum)
+
+def parttwo():
+    print("Advent of Code 2017, day 7, part 2.")
+    print("The answer is:", garb)
+
+partone()
+parttwo()
